1a7301219ad8d6d8d7cd5edf65817ee4c0526f0c
rafaeldutrabasso <rafaeldutrabasso@gmail.com>
2025-08-13 17:29:24 -0300

    Revert "Atualização completa para a nova versão do projeto"
    This reverts commit 99b14ebf4852b3126baece773693e32166701172.

--- Alimento.java
+++ Alimento.java
@@ -1,5 +1,13 @@
-package model;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appdieta;
 
+/**
+ *
+ * @author rafae
+ */
 public class Alimento {
     private String nome;
     private float quantidade;
